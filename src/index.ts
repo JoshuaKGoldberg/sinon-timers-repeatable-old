a79f3d29a9add085af668dace617692a23ccc26c
Josh Goldberg <git@joshuakgoldberg.com>
2022-08-13 16:13:17 -0400

    chore: add basic tests (#3)
    PR Checklist
    
     Addresses an existing open issue: fixes #2
     That issue was marked as accepting prs
     Steps in CONTRIBUTING.md were taken
    
    Overview
    Adds basic tests with Node's test runner.

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ declare const afterEach: ((callback: () => void) => void) | undefined;
 
 let clock: ReturnType<typeof sinon.useFakeTimers> | undefined;
 
-afterEach?.(() => {
-	clock?.reset();
-});
+if (typeof afterEach !== "undefined") {
+	afterEach?.(() => {
+		clock?.reset();
+	});
+}
 
 export function useFakeTimers(...args: Parameters<typeof sinon.useFakeTimers>) {
 	clock ??= sinon.useFakeTimers(...args);
